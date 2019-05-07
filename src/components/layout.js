9d130766022aa0c6223eb4ea8d813ce8a2afc0de
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-05-07 14:56:25 -0700

    Fixed issue where icon was rendering at a large size because render occurred before CSS loaded. Fixed by making sure CSS is loaded prior to rendering icons.

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,6 +16,12 @@ import Header from "./header"
 //import "./layout.css"
 import 'bootstrap/dist/css/bootstrap.css';
 
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false;
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
