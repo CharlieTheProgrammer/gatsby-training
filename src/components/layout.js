b71214dc924a49a5a9811a4479b253dc10876a1c
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-05-27 10:24:39 -0700

    Configured typography.js inline with layout.js. Otherwise, bootstrap will override the settings since it loads after gatsby-plugin-typography.

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,14 +14,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Location } from '@reach/router';
 
 import Header from "./header"
-//import "./layout.css"
 import 'bootstrap/dist/css/bootstrap.css'
 import typography from '../utils/typography'
+
 // This ensures that the icon CSS is loaded immediately before attempting to render icons
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 // Prevent fontawesome from dynamically adding its css since we did it manually above
 config.autoAddCss = false;
+
 typography.injectStyles()
 
 const Layout = ({ children }) => (
