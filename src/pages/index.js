418649fbeb7a60eaf646a614b8298a7ddc74ff7c
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-05-29 12:21:30 -0700

    Bug Fix: Accidentally left line of HTML commented out in last commit. Uncommented the line.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
       <h1 className="d-block d-sm-none d-none-lg d-none-xl h1 font-weight-light  text-center">I'm <br/> Charlie <br/> Ontiveros</h1>
       <p className="lead text-center font-weight-bold">I Solve Business Problems with Technical Solutions </p>
       <p className="d-none d-sm-block w-50">I'm a web application developer based in the Sacramento area, specializing in building high-quality NodeJs, Express, and SQL applications.</p>
-      {/* <p className="d-block d-sm-none d-none-lg d-none-xl w-75">I'm a web application developer based in the Sacramento area, specializing in building high-quality NodeJs, Express, and SQL applications.</p> */}
+      <p className="d-block d-sm-none d-none-lg d-none-xl w-75">I'm a web application developer based in the Sacramento area, specializing in building high-quality NodeJs, Express, and SQL applications.</p>
       <Link className="btn btn-outline-primary mb-4" to="/portfolio">Check Out My Projects</Link>
     </div>
   </Layout>
