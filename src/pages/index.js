f0652d1873018e516a6d0437fab3925f52e9ce97
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-06-07 09:16:11 -0700

    Testing previews on Netlify.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`Charlie Ontiveros`, `full stack application blog`]} />
     <div className="d-flex flex-column justify-content-center align-items-center text-center h-100">
-      <p className="d-none d-sm-block">Welcome to my site, I'm happy to see you here...</p>
+      <p className="d-none d-sm-block">Testing out previews</p>
       <h1 className="d-none d-sm-block display-4  text-center">I'm Charlie Ontiveros</h1>
       <h1 className="d-block d-sm-none d-none-lg d-none-xl h1 font-weight-light  text-center">I'm <br/> Charlie <br/> Ontiveros</h1>
       <p className="lead text-center font-weight-bold">I Solve Business Problems with Technical Solutions </p>
